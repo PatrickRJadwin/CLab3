d5b6e98e5ae4a77144ebc0751bd5d3889c2be991
Patrick <prjadwin@pstcc.edu>
2018-09-27 21:48:20 -0400

    First Commit

---
+++ Jadwin_Lab3/StdtScores.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Jadwin_Lab3
+{
+    public partial class stdtScores : Form
+    {
+        private static ArrayList stdtList = new ArrayList();
+        
+
+        public stdtScores()
+        {
+            InitializeComponent();
+        }
+        
+    }
+}
